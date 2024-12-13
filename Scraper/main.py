c109881bc5edd7ddd74a392b10a3e75fd1fc96dd
Maximilian Alexander Deichsel <devatwork.mad@gmail.com>
2024-12-13 17:54:03 +0100

    modified it into a reusable method

--- Scraper/main.py
+++ Scraper/main.py
@@ -12,12 +12,11 @@ options = ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=laptop&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=laptop")
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser")
-driver.quit()
+def gets_url(classes, location, source, url):
+    driver.get(url)
+    content = driver.page_source
+    soup = BeautifulSoup(content, "html.parser")
 
-def gets_url(classes, location, source):
     results = []
     for a in soup.findAll(attrs={"class": classes}):
         name = a.find(location)
@@ -25,14 +24,19 @@ def gets_url(classes, location, source):
             results.append(name.get(source))
     return results
 
-if __name__ == "__main__":
-    save_dir = Path(__file__).parent / "images"
+def scrape(dir, url):
+    save_dir = Path(__file__).parent / dir
     save_dir.mkdir(parents=True, exist_ok=True)
 
-    returned_results = gets_url("s-item__image-wrapper image-treatment", "img", "src")
+    returned_results = gets_url("s-item__image-wrapper image-treatment", "img", "src", url)
     for b in returned_results:
         image_content = requests.get(b).content
         image_file = io.BytesIO(image_content)
         image = Image.open(image_file).convert("RGB")
         file_path = save_dir / (hashlib.sha1(image_content).hexdigest()[:10] + ".png")
         image.save(file_path, "PNG", quality=80)
+
+if __name__ == "__main__":
+    scrape("images/1", "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=laptop&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=laptop")
+    scrape("images/2", "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=laptop&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=laptop")
+    driver.quit()
